3e289ce39b20136e7248c10acf20925fb9e4eb8c
Mar-cialobo <marcialobo-2@hotmail.com>
2025-04-30 21:33:23 -0300

    Atualiza estrutura do projeto e adiciona descrição no README

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,28 @@ import React from 'react';
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 // CSS
 import styles from './App.module.css';
+
+// Interface
+import { ITask } from './interfaces/Task';
 
 function App() {
   return (
     <div>
       <Header />
       <main className={styles.main}>
-        <h1>Conteúdo...</h1>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText='Criar tarefa' />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </div>
